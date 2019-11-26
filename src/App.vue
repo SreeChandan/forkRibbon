70ef79a0aedae49d21cc85cf483b0c0a72eeef7c
Sree Chandan <sreechandan98@gmail.com>
2019-11-27 01:57:23 +0530

    basic setup

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <ForkRibbon msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import ForkRibbon from "./components/ForkRibbon.vue";
+
+export default {
+  name: "app",
+  components: {
+    ForkRibbon
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ForkRibbon.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="forkRibbon">
+    <div class="inactive">
+      <div id="ribbon">Fork me on Github</div>
+    </div>
+    <div class="active">
+      <div id="cornerIcon">
+        <img :src="ImgSrc_Octocat" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ForkRibbon",
+  components: {},
+  props: {
+    toLink: String
+  },
+  data: function() {
+    return {};
+  },
+  computed: {
+    ImgSrc_Octocat: function() {
+      return require("@/assets/octocat.svg");
+    }
+  }
+};
+</script>
+
+<style scoped>
+#forkRibbon {
+  position: fixed;
+  /*right: 0px;
+  top: 0px;*/
+  top: 3.23em;
+  right: 3.23em;
+  transform: rotate(45deg);
+}
+div.active {
+  display: block;
+}
+div.inactive {
+  display: none;
+}
+
+div#ribbon {
+  position: absolute;
+  right: -100px;
+  width: 200px;
+  height: 25px;
+  background-color: darkgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
